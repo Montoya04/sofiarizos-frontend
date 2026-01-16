3a11883e29fafc3021ad16c6fb9e93de9504b201
Montoya04 <alejandromontoya412@gmail.com>
2026-01-16 16:11:04 -0500

    Fix admin login routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Layout
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+// Páginas públicas
 import Servicios from "./pages/Servicios";
 import Cursos from "./pages/Cursos";
 import Reserva from "./pages/Reserva";
 import Carrito from "./pages/Carrito";
-import ReservaAdmin from "./pages/ReservaAdmin";
+
+// Admin
 import AdminLogin from "./pages/AdminLogin";
+import ReservaAdmin from "./pages/ReservaAdmin";
 import AdminGuard from "./components/AdminGuard";
 
-export default function App() {
+// Layout público (con header y footer)
+function PublicLayout() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Servicios />} />
         <Route path="/cursos" element={<Cursos />} />
         <Route path="/reserva" element={<Reserva />} />
         <Route path="/carrito" element={<Carrito />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
 
-        {/* LOGIN ADMIN */}
-        <Route path="/admin/login" element={<AdminLogin />} />
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* LOGIN ADMIN (SIN HEADER NI FOOTER) */}
+        <Route path="/admin-login" element={<AdminLogin />} />
 
         {/* PANEL ADMIN PROTEGIDO */}
         <Route
@@ -32,8 +47,10 @@ export default function App() {
             </AdminGuard>
           }
         />
+
+        {/* SITIO PÚBLICO */}
+        <Route path="/*" element={<PublicLayout />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
