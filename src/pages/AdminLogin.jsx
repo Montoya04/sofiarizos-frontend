545a1fc64c4479a569e1d17f5d39f6aa933ed986
Montoya04 <alejandromontoya412@gmail.com>
2026-01-16 15:38:53 -0500

    Cambios en AdminLogin.jsx

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -15,7 +15,7 @@ export default function AdminLogin() {
     setError("");
 
     try {
-      const res = await fetch(`${API_URL}/auth/login`, {
+      const res = await fetch(`${API_URL}/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: username, password }),
