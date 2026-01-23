70cd4976d4ad6c799aab4c88556ad68a7840b501
Montoya04 <alejandromontoya412@gmail.com>
2026-01-23 16:19:56 -0500

    Cambios en las rutas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -11,14 +13,22 @@ import AdminLogin from "./pages/AdminLogin";
 import AdminGuard from "./components/AdminGuard";
 
 export default function App() {
+  const [carrito, setCarrito] = useState([]); // ✅ AQUÍ ESTÁ LA CLAVE
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Servicios />} />
-        <Route path="/cursos" element={<Cursos />} />
+
+        <Route
+          path="/cursos"
+          element={<Cursos carrito={carrito} setCarrito={setCarrito} />}
+        />
+
+        <Route path="/carrito" element={<Carrito carrito={carrito} />} />
+
         <Route path="/reserva" element={<Reserva />} />
-        <Route path="/carrito" element={<Carrito />} />
 
         {/* LOGIN ADMIN */}
         <Route path="/admin/login" element={<AdminLogin />} />
